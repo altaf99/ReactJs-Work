e12b1fac187d3acfa0e979813b2b3c6342785b2a
altaf99 <iamaltafshaikh07@gmail.com>
2019-09-28 10:46:45 +0530

    Used props using functional component using Greet.js

--- src/App.js
+++ src/App.js
@@ -10,9 +10,18 @@ function App() {
     <div className="App">
     {/* to include Greet component into app.js we simply need to create
     coustom html tag */}
-    {/* <Greet /> 
-    <Welcome /> */}
-    <Hello></Hello>
+    <Greet name="Altaf" heroname="Iron Man">
+      This is Children Props.
+      Pass Data In between this when you have no clue what is
+      going to pass as props.
+    </Greet> 
+    {/* name="Altaf" means passing parameter or props */}
+    <Greet name="Diana" heroname="Bat Women">
+    <button>Action</button>
+    </Greet>
+    <Greet name="Emma" heroname="Black Lady" /> 
+    {/* <Welcome /> */}
+    {/* <Hello></Hello> */}
     
     </div>
   );
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -6,9 +6,24 @@ import React from 'react'
 //     return <h1>Hello Altaf</h1>
 // }
 
-const Greet = () => <h1>Hello Altaf</h1>
+//add a parameter to the function to catch the name
+//i.e props u can named it anything
+//use this keyword to access the value
+const Greet = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Hello, {props.name} a.k.a {props.heroname}</h1>
+            {props.children}
+        </div>
+    ) //add () to split the JSX in multiple lines and wrap in single div tag
+} 
 export default Greet //using this we can import Greet with any name
+
+
 //named export=> export const Greet = () => <h1>Hello Altaf</h1>
 // now we need to connect this component with the main parent,
 // so go to applicationCache.js
 
+// props means => optional input or properties
+// it makes the component dynamic
