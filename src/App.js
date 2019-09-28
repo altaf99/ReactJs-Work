f0e2d282d3b07b45483dab36ded0393830776caf
altaf99 <iamaltafshaikh07@gmail.com>
2019-09-28 10:12:00 +0530

    First Class component created

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
-
+import Welcome from './components/Welcome'
 function App() {
   return (
     <div className="App">
     {/* to include Greet component into app.js we simply need to create
     coustom html tag */}
     <Greet /> 
-      
+    <Welcome />
     </div>
   );
 }
---
+++ src/components/Welcome.js
@@ -0,0 +1,13 @@
+//creating Class component
+import React, { Component } from 'react'
+
+//extend the class with React component Class to make it react component
+class Welcome extends Component{
+    render() {
+        return <h1>Class Component</h1>
+    }
+}
+
+export default Welcome
+
+//class has to render()
