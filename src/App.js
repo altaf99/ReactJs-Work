7d7a12e490c0174b41705a580c6cad5b593ba38b
altaf99 <iamaltafshaikh07@gmail.com>
2019-09-28 10:01:11 +0530

    First Functional component created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* to include Greet component into app.js we simply need to create
+    coustom html tag */}
+    <Greet /> 
+      
     </div>
   );
 }
---
+++ src/components/Greet.js
@@ -0,0 +1,12 @@
+// this this the first functional component
+
+import React from 'react'
+
+function Greet(){
+    return <h1>Hello Altaf</h1>
+}
+
+export default Greet
+
+// now we need to connect this component with the main parent,
+// so go to applicationCache.js
