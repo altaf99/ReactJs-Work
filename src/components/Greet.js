cb04607c4398f5e1517260fc67a3a7732a8d04f3
altaf99 <iamaltafshaikh07@gmail.com>
2019-09-28 10:05:03 +0530

    Functional component modify using arrow function of ES6 syntax

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -2,11 +2,13 @@
 
 import React from 'react'
 
-function Greet(){
-    return <h1>Hello Altaf</h1>
-}
+// function Greet(){
+//     return <h1>Hello Altaf</h1>
+// }
 
-export default Greet
-
+const Greet = () => <h1>Hello Altaf</h1>
+export default Greet //using this we can import Greet with any name
+//named export=> export const Greet = () => <h1>Hello Altaf</h1>
 // now we need to connect this component with the main parent,
 // so go to applicationCache.js
+
